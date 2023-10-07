d269a21d12945b988d5422f5264d3e4497dfcf03
Leandro <1190800@isep.ipp.pt>
2023-10-07 21:03:46 +0100

    correcoes

--- public/app/src/modules/forum/components/posts/postRow/components/PostRow.tsx
+++ public/app/src/modules/forum/components/posts/postRow/components/PostRow.tsx
@@ -5,17 +5,20 @@ import { Post } from '../../../../models/Post';
 import { Points } from '../../points';
 import PostMeta from '../../post/components/PostMeta';
 import { now } from 'moment';
+import { PostFilterType } from '../../filters/components/PostFilters';
 
 interface PostRowProps extends Post {
   onUpvoteClicked: () => void;
   onDownvoteClicked: () => void;
   isLoggedIn: boolean;
+  filter: PostFilterType;
 }
 
 const PostRow: React.FC<PostRowProps> = (props) => (
   <div className="post-row" style={
     {
-    backgroundColor: new Date(props.createdAt).toDateString() == new Date().toDateString() ? 'antiquewhite' : 'none'
+    backgroundColor: new Date(props.createdAt).toDateString() == new Date().toDateString() 
+    && props.filter == 'NEW' ? 'antiquewhite' : '#F9F9F9'
   }
     }>
     <Points
